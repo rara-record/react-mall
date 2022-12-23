29ef8816552579ff94525281c635ad506acc94c3
BORA <a52447879@gmail.com>
2022-12-23 22:21:52 +0900

    feat: 어드민 사용자 만들기

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from 'firebase/auth'
-
+import { getDatabase, get, ref, onValue } from 'firebase/database'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,6 +11,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth()
 const provider = new GoogleAuthProvider()
+const db = getDatabase(app)
 
 export const login = () => {
   signInWithPopup(auth, provider).catch(console.error)
@@ -21,7 +22,27 @@ export const logout = () => {
 }
 
 export const onUserStateChange = (callback) => {
-  onAuthStateChanged(auth, (user) => {
-    callback(user)
+  onAuthStateChanged(auth, async (user) => {
+    console.log(user)
+    const updatedUser = user ? await adminUser(user) : null
+    callback(updatedUser)
   })
+}
+
+/**
+ * firebase ref: https://firebase.google.com/docs/database
+ * @description: 어드민 사용자를 판별하고, 객체에 결과값을 추가하여 리턴하는 함수
+ * @param user
+ * @returns {Promise<DataSnapshot>}
+ */
+const adminUser = async (user) => {
+  return get(ref(db, 'admins')) //
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const admins = snapshot.val()
+        const isAdmin = admins.includes(user.uid)
+        return { ...user, isAdmin }
+      }
+      return user
+    })
 }
