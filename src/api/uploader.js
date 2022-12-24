78c0db187b43459607d24ef06b34973512ce4e78
BORA <a52447879@gmail.com>
2022-12-24 21:20:41 +0900

    coment: fix coment

--- src/api/uploader.js
+++ src/api/uploader.js
@@ -2,7 +2,7 @@
  *
  * @param file: 제품 등록 이미지
  * @returns {Promise<any>}
- * @description cloudinary에 제품을 등록하는 함수
+ * @description cloudinary에 제품을 등록하고, url을 반환하는 함수
  */
 export const uploadImage = (file) => {
   const formData = new FormData()
