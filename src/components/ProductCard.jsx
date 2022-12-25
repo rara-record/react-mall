794c905b7c8ca86771aa60887bd803f67ee8df31
BORA <a52447879@gmail.com>
2022-12-25 21:32:28 +0900

    feat: add product detail component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const ProductCard = ({ product: { id, image, title, category, price } }) => {
+const ProductCard = ({ product, product: { id, image, title, category, price } }) => {
+  const navigate = useNavigate()
+
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
-      <img className="w-full" src={image} alt={title} />
+    <li
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer"
+      onClick={() => navigate(`/products/${id}`, { state: { product } })}
+    >
+      <img className="w-full transition-all hover:scale-105" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex justify-between items-center">
         <h3 className="truncate">{title}</h3>
         <p>{`₩${price}`}</p>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function ProductDetail() {
-  return <div>Product Details</div>;
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation()
+
+  return <div>{id}</div>
 }
