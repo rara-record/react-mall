4e536b1424778b3ddcef93f6150e8d767e0e9a64
BORA <a52447879@gmail.com>
2022-12-27 22:56:20 +0900

    design: 상세페이지 style

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -20,20 +20,31 @@ const ProductDetail = () => {
   const onAddCart = (e) => {}
 
   return (
-    <section>
-      <p>{category}</p>
-      <img src={image} alt={title} />
-      <div>
-        <h2>{title}</h2>
-        <p>₩{price}</p>
-        <p>{description}</p>
-        <p>옵션:</p>
-        <select onChange={onChangeSelect} value={selected}>
-          {options && options.map((option, idx) => <option key={idx}>{option}</option>)}
-        </select>
-      </div>
-      <Button onClick={onAddCart}>장바구니에 추가</Button>
-    </section>
+    <>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <img className="w-full px-4 basis-7/12" src={image} alt={title} />
+        <div className="w-full basis-5/12 flex flex-col p-4">
+          <h2 className="text-3xl font-bold py-2">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-gray-400">₩{price}</p>
+          <p className="py-4 te  xt-lg">{description}</p>
+          <div className="flex items-center">
+            <label className="text-brand font-bold" htmlFor="select">
+              옵션:
+            </label>
+            <select
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              id="select"
+              onChange={onChangeSelect}
+              value={selected}
+            >
+              {options && options.map((option, idx) => <option key={idx}>{option}</option>)}
+            </select>
+          </div>
+          <Button onClick={onAddCart}>장바구니에 추가</Button>
+        </div>
+      </section>
+    </>
   )
 }
 
